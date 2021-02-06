e949afc51acf260bdac73e54a72d7b02d8e1d53f
Taylor Kolkmeyer <tkolkmeyer@gmail.com>
2021-02-05 18:21:52 -0600

    AbstractEntity completed

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -1,13 +1,28 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotBlank(message = "Name is required.")
+    @Size(min = 3, max = 200, message ="Name must be between 3 and 200 characters.")
     private String name;
+
+    public AbstractEntity(@NotBlank(message = "Name is required.") @Size(min = 3, max = 200, message = "Name must be between 3 and 200 characters.") String name) {
+        this.name = name;
+    }
+
+    public AbstractEntity(){}
 
     public int getId() {
         return id;
