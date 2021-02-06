30ece8b788e7d1e386d4f35f930df06cabbe7945
Taylor Kolkmeyer <tkolkmeyer@gmail.com>
2021-02-05 18:36:57 -0600

    finished employer class

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(min=3, max=200, message="Must be between 3 and 200 characters.")
+    private String location;
 
+    public Employer(@NotBlank(message = "Name is required.") @Size(min = 3, max = 200, message = "Name must be between 3 and 200 characters.") String name, @NotBlank @Size(min = 3, max = 200, message = "Must be between 3 and 200 characters.") String location) {
+        super();
+        this.location = location;
+    }
+
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
